ae7e36f56298e49bfb33a8a8cd538ef4f432a546
krishna <krishnasabu885@gmail.com>
2024-02-28 11:40:43 +0530

    firstcommit

--- vite-project/src/components/adminlogin/Adminloginpage.jsx
+++ vite-project/src/components/adminlogin/Adminloginpage.jsx
@@ -1,7 +1,20 @@
 import React,{useState} from "react";
+import axios from "axios";
 import './adminlogin.css'
 
+
+
+
 function Adminloginpage() {
+
+  function handlelogin() {
+
+    const HOSTED_SERVER_URL =urls()
+
+    const response = axios.post(`$(HOSTED_SERVER_URL)/login`,values)
+    
+  }
+
     return (
 <>
 <meta charSet="UTF-8" />
@@ -11,7 +24,7 @@ function Adminloginpage() {
   <form >
     <h2 className="heading">Admin login</h2>
     <label htmlFor="email">Admin Name:</label>
-    <input type="email" id="login_email" name="login_email" required="" />
+    <input type="email" id="login_email" name="login_email"  required="" />
     <label htmlFor="password">Password:</label>
     <input
       type="password"
@@ -19,9 +32,7 @@ function Adminloginpage() {
       name="login_password"
       required=""
     />
-    <button   className="but_admin" type="button">
-      SUBMIT
-    </button>
+    <button   className="but_admin" onClick={handlelogin()}  type="button">SUBMIT </button>
   </form>
 </>
     )
